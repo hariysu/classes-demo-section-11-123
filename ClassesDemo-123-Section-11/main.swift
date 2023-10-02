982a38f0975a75ebc5b15e31452f775411a7dfa8
Abdurahman AYDIN <hariysu@gmail.com>
2023-10-02 17:21:17 +0300

    Shows the main differences between class and struct

--- ClassesDemo-123-Section-11/main.swift
+++ ClassesDemo-123-Section-11/main.swift
@@ -7,7 +7,19 @@
 
 import Foundation
 
-print("Hello, World!")
+// The main differences between class and struct are:
+//      STRUCT      =>      IMMUTABLE,      PASSED BY VALUE
+//      CLASS       =>      INHERITANCE,    PASSED BY REFERENCE
+
+// Passed by reference
+let skeleton1 = ClassEnemy(health: 100, attackStrength: 10)
+var skeleton2 = skeleton1
+
+skeleton1.takeDamage(amountOfDamage: 10)
+skeleton2.takeDamage(amountOfDamage: 20)
+
+print(skeleton1.health)             //  70
+print(skeleton2.health)             //  70
 
 
 
