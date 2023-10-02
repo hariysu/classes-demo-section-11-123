d9df786cc5b7e56a8c8211e37bdd47099d945576
Abdurahman AYDIN <hariysu@gmail.com>
2023-10-02 17:37:52 +0300

    Shows the differences between passed by value and passed by reference

--- ClassesDemo-123-Section-11/main.swift
+++ ClassesDemo-123-Section-11/main.swift
@@ -12,6 +12,7 @@ import Foundation
 //      CLASS       =>      INHERITANCE,    PASSED BY REFERENCE
 
 // Passed by reference
+// We can use both var and let for class objects when a property of object changes
 let skeleton1 = ClassEnemy(health: 100, attackStrength: 10)
 var skeleton2 = skeleton1
 
@@ -21,5 +22,21 @@ skeleton2.takeDamage(amountOfDamage: 20)
 print(skeleton1.health)             //  70
 print(skeleton2.health)             //  70
 
+
+
+// Passed by value
+// We can use only var for struct objects when a property of object changes
+var skeleton3 = StructEnemy(health: 100, attackStrength: 10)
+var skeleton4 = skeleton3
+
+skeleton3.takeDamage(amountOfDamage: 10)
+skeleton4.takeDamage(amountOfDamage: 20)
+
+print(skeleton3.health)             //  90
+print(skeleton4.health)             //  80
+
+
+// We can consider struct object is a sample of an object, but class object is reference of an object.
+// If we change struct object, we would changed only this sample. But we change class object, we would changed every same object
 
 
